5899232f1972ee601827eda7171955e62f34967b
ralph2308s <ralph2308s@gmail.com>
2024-03-30 14:58:54 +0700

    fix: icon-switch-mode

--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,4 +1,6 @@
 import UseDarkSide from "../themes/UseDarkSide";
+import iconLightMode from "../assets/icons/icon-light-mode_32_light.svg";
+import iconDarkMode from "../assets/icons/icon-dark-mode_32_dark.svg";
 
 export default function Switcher() {
   const [nextTheme, setTheme] = UseDarkSide();
@@ -6,13 +8,13 @@ export default function Switcher() {
   return (
     <>
       <img
-        src="./src/assets/icons/icon-light-mode_32_light.svg"
+        src={iconLightMode}
         alt="icon-light-mode_light"
         className="dark:hidden"
         onClick={() => setTheme(nextTheme)}
       />
       <img
-        src="./src/assets/icons/icon-dark-mode_32_dark.svg"
+        src={iconDarkMode}
         className="hidden dark:block"
         alt="icon-light-mode_light"
         onClick={() => setTheme(nextTheme)}
